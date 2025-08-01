6f4ac14a659345a4b07cf44363460585de9fd6cd
Bogdan Kokh <creative.bogdan.kokh@renue.ru>
2025-08-02 02:37:44 +0500

    fix path to svg

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import unfocused from '../svg/unfocused.svg';
+import unfocused from './svg/unfocused.svg';
 
-import closeDefault from '../svg/close/default.svg';
-import closeHover from '../svg/close/hover.svg';
-import closeActive from '../svg/close/active.svg';
+import closeDefault from './svg/close/default.svg';
+import closeHover from './svg/close/hover.svg';
+import closeActive from './svg/close/active.svg';
 
-import minimizeDefault from '../svg/minimize/default.svg';
-import minimizeHover from '../svg/minimize/hover.svg';
-import minimizeActive from '../svg/minimize/active.svg';
+import minimizeDefault from './svg/minimize/default.svg';
+import minimizeHover from './svg/minimize/hover.svg';
+import minimizeActive from './svg/minimize/active.svg';
 
-import maximizeDefault from '../svg/maximize/default.svg';
-import maximizeHover from '../svg/maximize/hover.svg';
-import maximizeActive from '../svg/maximize/active.svg';
+import maximizeDefault from './svg/maximize/default.svg';
+import maximizeHover from './svg/maximize/hover.svg';
+import maximizeActive from './svg/maximize/active.svg';
 
 export {
     unfocused,
